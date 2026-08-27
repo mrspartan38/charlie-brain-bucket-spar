2852bc6bd3ad29400102061d2b43c55fc53f639a
Sager <esager@una.edu>
2026-08-27 13:31:09 -0500

    public html branch

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 import express from 'express'
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
-const app = express()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const app = express();
+
+app.use(express.static(join(__dirname, 'public', 'index.html')));
 
 app.get('/', (req, res) => {
   res.send('Hello World')
